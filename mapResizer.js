b1d4f529405710ef1d2f11ebf27128eb3db51519
YeemBoi <47729640+YeemBoi@users.noreply.github.com>
2020-02-17 17:34:00 -0800

    bruh moment

--- mapResizer.js
+++ mapResizer.js
@@ -1,24 +1,29 @@
-var mappedImages = maps = ocs = natWidths = widths = areas = [];
+var mappedImages = [];
+var maps = [];
+var ocs = [];
+var natWidths = [];
+var areas = [];
+var allImages;
 var mapI = 0;
 var i, j;
 
 window.onload = function()
 {
-	var allImages = document.getElementsByTagName("IMG");
+	allImages = document.getElementsByTagName("IMG");
 	for (i=0; i<allImages.length; i++)
 	{
 		if (allImages[i].hasAttribute("USEMAP"))
 		{
-			var cMap = document.getElementsByName(allImages[i].useMap.slice(1));
+			var cMap = document.getElementsByName(allImages[i].useMap.slice(1))[0];
 			if (cMap)
 			{
-				cMap = cMap[0];
 				ocs.push([]);
 				natWidths.push(allImages[i].naturalWidth);
-				widths.push(allImages[i].width);
 				areas.push(cMap.getElementsByTagName("AREA"));
 				for (j=0; j<areas[mapI].length; j++)
-					ocs[mapI].push(areas[mapI][j].coords.split(",").map(Number)); // the OTHER kind of map
+				{
+				    ocs[mapI].push(areas[mapI][j].coords.split(",").map(Number)); // the OTHER kind of map
+				}
 				maps.push(cMap);
 				mapI++;
 			}
@@ -33,6 +38,6 @@ function redoMaps()
 	for (i=0; i<mapI; i++)
 	{
 		for (j=0; j<areas[i].length; j++)
-			areas[i][j].coords = ocs[i][j].map(x => x / natWidths[i] * widths[i]).join();
+			areas[i][j].coords = ocs[i][j].map(x => x / natWidths[i] * allImages[i].width).join();
 	}
 }
